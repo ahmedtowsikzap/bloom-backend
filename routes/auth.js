8695a9d43b919cddf9d9174765732135be721e08
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-14 22:09:13 +0600

    hasing password with crypto & aes encryption to secure password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const User = require("../models/User")
+const User = require("../models/User");
+const CryptoJS = require("crypto-js")
 // REGISTER
 
 router.post("/register", async (req,res) => {
@@ -7,7 +8,7 @@ router.post("/register", async (req,res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     });
     try{
         const savedUser = await newUser.save();
