302398b009bea85d73457c3ab242cd9a1d95c60c
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-14 23:03:55 +0600

    small changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-const userRoute = require("./routes/users")
+const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 
 dotenv.config()
