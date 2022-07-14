f994c1535caca07bd584c638d3f07e3698eb264a
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-14 13:46:37 +0600

    env file configuration

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const express = require("express")
+const app = express();
+const mongoose = require("mongoose")
+
+
+
+app.listen(5000, ()=>{
+    console.log("backend runnnig")
+})
