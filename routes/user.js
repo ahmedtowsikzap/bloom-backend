6203d502bea04f33f46c2cb986084fb8905c1d96
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-14 23:32:59 +0600

    verifytoken for updating user &

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,21 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
-router.put("/:id", )
+router.put("/:id",  verifyTokenAndAuthorization,  async (req, res)=>{
+  
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    }
+
+    try{
+       const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+
+        $set: req.body
+       }, {new:true})
+       res.status(200).json(updatedUser)
+    }catch(err){res.status(500).json(err)}
+})
 
 module.exports = router
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken")
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token
     if(authHeader){
+        const token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.JWT_SEC, (err, user)=>{
             if(err) res.status(403).json("The token is not valid");
             req.user = user;
@@ -21,7 +22,17 @@ const verifyTokenAndAuthorization = (req,res, next) => {
 
             res.status(403).json("Your are not eligible to this operation");
         }
+    });
+};
+const verifyTokenAndAdmin = (req,res, next) => {
+    verifyToken(req,res, ()=> {
+        if(req.user.isAdmin){
+        next()
+        }else{
+
+            res.status(403).json("Your are not eligible to this operation");
+        }
     })
 }
 
-module.exports = {verifyToken} 
+module.exports = {verifyToken,  verifyTokenAndAuthorization, verifyTokenAndAdmin } 
