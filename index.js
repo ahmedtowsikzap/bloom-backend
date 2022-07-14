94cf66e5dec6317a0590efc43538f90fa2d09f80
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-14 16:23:32 +0600

    mongourl database connection & .env file setup for privacy

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express")
 const app = express();
 const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
+dotenv.config()
+
+
+mongoose.
+connect(
+  process.env.MONGO_URL
+).then(()=> console.log("connection success"))
+
+.catch((err) => {
+    console.log(err)
+});
 
 
 app.listen(5000, ()=>{
