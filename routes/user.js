36c996a72c7592226dd2374b4250b1c5840c5788
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-14 23:48:16 +0600

    Get user functionality

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
 
@@ -33,4 +33,16 @@ router.delete("./:id" , verifyTokenAndAuthorization , async (req,res) => {
     }
 })
 
+// GET USER
+
+router.get("./find/:id" , verifyTokenAndAdmin , async (req,res) => {
+
+    try{
+        const user = await User.findById(req.params.id)
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 module.exports = router
