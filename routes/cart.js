1ba28afef93c88f7227c381df5b43a514585d7ac
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-17 21:26:23 +0600

    get all cart stuff with admin authorization

--- routes/cart.js
+++ routes/cart.js
@@ -64,4 +64,17 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
     }
   });
 
+////// GET ALL
+
+router.get("/", verifyTokenAndAdmin, async (req, res) =>{
+    try{
+        const carts = await Cart.find()
+        res.status(200).json(carts)
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+
+})
+
 module.exports = router
