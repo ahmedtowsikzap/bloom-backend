89d0c148307c86473fb8047f9ec8ba79a5328f9f
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-17 18:21:49 +0600

    create product itmes using admin request & authorization

--- routes/product.js
+++ routes/product.js
@@ -5,9 +5,16 @@ const router = require("express").Router();
 
 // CREATE
 
-router.post("/", async (req,res) => {
+router.post("/", verifyTokenAndAdmin,  async (req,res) => {
+ 
+        const newProduct = new Product(req.body)
+        try{
+            const savedProduct = await newProduct.save();
+            res.status(200).json(savedProduct);
 
-    
+        }catch(err){
+            res.status(500).json(err)
+        }
 })
 
 
