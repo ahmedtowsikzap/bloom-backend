f9d07f3bc603335f1770cd026feb373b0cb5abcc
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-18 00:02:03 +0600

    stripe integration & added to stripe route to index js

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
 const cartRoute = require("./routes/cart")
 const orderRoute = require("./routes/order")
-
+const stripeRoute = require("./routes/stripe")
+const cors = require("cors")
 dotenv.config()
+
+
 
 
 mongoose.
@@ -20,11 +23,13 @@ connect(
     console.log(err)
 });
 
+app.use(cors());
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/products", productRoute)
 app.use("/api/carts", cartRoute)
 app.use("/api/orders", orderRoute)
+app.use("/api/checkout", stripeRoute)
 
 
 
