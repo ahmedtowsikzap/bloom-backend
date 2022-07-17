21c95b34447c17c3044dfe21fa4bce1e92bfaa11
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-17 19:04:57 +0600

    Get all products info

--- routes/product.js
+++ routes/product.js
@@ -55,43 +55,30 @@ router.get("/find/:id", async (req,res) => {
         res.status(500).json(err)
     }
 });
-// GET All Users
+// GET All PRODUCTS INFORMATION
 
-router.get("/" , verifyTokenAndAdmin , async (req,res) => {
-   const query = req.query.new
-    try{
-        const users = query ?  await user.find().sort({_id: -1}).limit(6) :  await User.find()
-        res.status(200).json(users);
-    }catch(err){
-        res.status(500).json(err)
+router.get("/", async (req, res) => {
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
+    try {
+      let products;
+  
+      if (qNew) {
+        products = await Product.find().sort({ createdAt: -1 }).limit(5);
+      } else if (qCategory) {
+        products = await Product.find({
+          categories: {
+            $in: [qCategory],
+          },
+        });
+      } else {
+        products = await Product.find();
+      }
+  
+      res.status(200).json(products);
+    } catch (err) {
+      res.status(500).json(err);
     }
-});
-
-// GET USER STATS
-
-router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-   const date = new Date();
-   const lastYear = new Date(date.setFullYear(date.getFullYear() -1));
-
-   try{
-    const data = await User.aggregate([])
-        {$match: {createdAt: {$gte: lastYear} } }
-        {
-            $project:{
-                month: {$month: "$createdAt"}
-            }
-        }
-        {
-            $group: {
-
-                _id: "$month"
-                total: { $sum: 1}
-            }
-        }
-       res.status(200).json(data)
-   } catch(err){
-    res.status(500).json(err);
-   }
-
-})
+  });
+  
 module.exports = router
