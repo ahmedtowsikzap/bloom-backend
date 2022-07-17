54a93218ad190a24f8211813c8226f3a398e5642
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-17 21:28:32 +0600

    small chnages & fix

--- routes/cart.js
+++ routes/cart.js
@@ -1,4 +1,4 @@
-const Product = require("../models/Product");
+const Cart = require("../models/Cart");
 const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
