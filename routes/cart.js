44f1db9ba88ff45d86675f84dee8e9266307cc19
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-17 19:15:28 +0600

    cart verifying with JWT token

--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,20 @@
+const Product = require("../models/Product");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
 const router = require("express").Router();
 
+// CREATE
 
+router.post("/", verifyToken,  async (req,res) => {
+ 
+        const newCart = new Cart(req.body)
+        try{
+            const savedCart = await newCart.save();
+            res.status(200).json(savedCart);
 
+        }catch(err){
+            res.status(500).json(err)
+        }
+})
+  
 module.exports = router
