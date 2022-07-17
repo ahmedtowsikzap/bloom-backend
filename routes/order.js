b605169f615df6bd4df991c52c27d3f754fbdcb2
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-17 21:43:30 +0600

    get all for admin

--- routes/order.js
+++ routes/order.js
@@ -52,23 +52,12 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
       res.status(500).json(err);
     }
   });
-  
-  // //GET ALL USERCART 
-
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
-    try {
-      const carts = await Cart.find();
-      res.status(200).json(carts);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
 
 ////// GET ALL
 
 router.get("/", verifyTokenAndAdmin, async (req, res) =>{
     try{
-        const carts = await Cart.find()
+        const orders = await Order.find()
         res.status(200).json(carts)
     }catch(err){
         res.status(500).json(err)
