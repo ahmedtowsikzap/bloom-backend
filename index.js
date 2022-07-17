1203302e101b593b1f0c96c3788f9e3918c8b729
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-17 23:06:39 +0600

    cart & product route added to index

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
 
 dotenv.config()
 
@@ -21,6 +23,8 @@ connect(
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/products", productRoute)
+app.use("/api/carts", cartRoute)
+app.use("/api/orders", orderRoute)
 
 
 
