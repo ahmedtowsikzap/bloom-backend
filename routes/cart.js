72c961e067b6ac96f5c22e425adddf842ea51388
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-17 20:22:48 +0600

    Update cart functionality

--- routes/cart.js
+++ routes/cart.js
@@ -16,5 +16,19 @@ router.post("/", verifyToken,  async (req,res) => {
             res.status(500).json(err)
         }
 })
+
+// UPDATE 
+
+router.put("/:id",  verifyTokenAndAuthorization,  async (req, res) => {
+
+    try{
+       const updatedCart = await Cart.findByIdAndUpdate(
+        req.params.id,
+         {
+        $set: req.body
+       }, {new:true})
+       res.status(200).json(updatedCart)
+    }catch(err){res.status(500).json(err)}
+});
   
 module.exports = router
