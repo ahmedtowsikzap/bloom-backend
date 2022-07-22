8202e005792e7fb240ebc3deba030b9dba4653d9
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-22 16:10:26 +0600

    changed login codes

--- routes/auth.js
+++ routes/auth.js
@@ -24,45 +24,35 @@ router.post("/register", async (req, res) => {
 
 //LOGIN
 
-router.post('/login', async (req, res) => {
-    try{
-        const user = await User.findOne(
-            {
-                userName: req.body.user_name
-            }
-        );
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials!");
 
-        !user && res.status(401).json("Wrong User Name");
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-        const hashedPassword = CryptoJS.AES.decrypt(
-            user.password,
-            process.env.PASS_SEC
-        );
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials!");
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      {expiresIn:"3d"}
+    );
 
-        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    const { password, ...others } = user._doc;
 
-        const inputPassword = req.body.password;
-        
-        originalPassword != inputPassword && 
-            res.status(401).json("Wrong Password");
-
-        const accessToken = jwt.sign(
-        {
-            id: user._id,
-            isAdmin: user.isAdmin,
-        },
-        process.env.JWT_SEC,
-            {expiresIn:"3d"}
-        );
-  
-        const { password, ...others } = user._doc;  
-        res.status(200).json({...others, accessToken});
-
-    }catch(err){
-        res.status(500).json(err);
-    }
-
+    res.status(200).json({...others, accessToken});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
